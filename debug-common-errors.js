b9d0db01af3eff4f3f277c3b7d001207f8308c14
supernovabee <dosin.1ne@gmail.com>
2025-07-20 23:32:16 -0500

    added my code and explainations of fixes

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,23 +24,24 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp");
 
 // What’s Wrong?
-
+//There is no closing parenthesis, closing quotations,  or ending semicolon. Adding those to fix. 
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4, Number("8")];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
 // What’s Wrong?
-
+// Eight is defined as a string with characters, not a number. It is throwing up a NaN error since it can't be calculated.
+//I kept the value as a string so I could use Number to convert it to an integer. Alongside that, I changed eight to 8 for easy conversion.
 
 
 // Program C (Logic Error)
@@ -51,12 +52,14 @@ function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
-console.log(isPrime(7)); // Expected true but gets false
+console.log(isPrime(257)); // Expected true but gets false
 
 // What’s Wrong?
+//the False and True values in the 2nd and 3rd return were flipped.
+//switched them around and tested code for functionality with multiple prime and not prime numbers.
